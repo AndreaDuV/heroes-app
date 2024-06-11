c679ec87864634a1909aea7832966c7c40243e6d
AndreaDuV <andreas27job@gmail.com>
2024-06-11 18:01:13 -0500

    seccion 16 - guards en auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorPageComponent } from './shared/pages/error-page/error-page.component';
+import { AuthGuard } from './auth/guards/auth.guard';
+import { PublicGuard } from './auth/guards/public.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canMatch: [ PublicGuard ],
+    canActivate: [ PublicGuard ],
   },
   {
     path: 'heroes',
     loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule),
+    canActivate: [ AuthGuard ],
+    canMatch: [ AuthGuard ],
   },
   {
     path: '404',
